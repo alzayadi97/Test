c8857452e9c5321da2c7e4e7dbe8ce065c05fa8b
Mort <alzerd97@gmail.com>
2024-03-07 15:51:43 -0500

    2

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,8 +3,8 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 2;
-		int b = 4;
+		int a = 0;
+		int b = 1;
 		int c = 0;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
